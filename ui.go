d1380c8e54280a9dd83d422876b40a4b71951ebb
Terrence Ryan <tpryan@google.com>
2022-10-31 17:00:12 -0700

    fix: rolling back bad change.

--- ui.go
+++ ui.go
@@ -360,7 +360,7 @@ func RegionsList(project, product string) ([]string, error) {
 		return regionsRun(project)
 	}
 
-	return []string{}, fmt.Errorf("invalid product (%s) requested: %s ", product, err)
+	return []string{}, fmt.Errorf("invalid product (%s) requested", product)
 }
 
 // RegionManage promps a user to select a region.
