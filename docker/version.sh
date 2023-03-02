d4c7cd9ec87cb6a298ae2a214b7fbc7569704987
Terrence <tpryan@google.com>
2023-03-02 14:56:50 -0800

    fix: trying to get new verison working.

--- docker/version.sh
+++ docker/version.sh
@@ -1,4 +1,6 @@
-cat go.mod| grep github.com/GoogleCloudPlatform/deploystack | head -1 | sed  's/github.com\/GoogleCloudPlatform\///g' | xargs  > dsVersion
-cat go.mod| grep github.com/GoogleCloudPlatform/deploystack/tui  |  sed  's/github.com\/GoogleCloudPlatform\///g' | xargs > dsTUIVersion
-cat go.mod| grep github.com/GoogleCloudPlatform/deploystack/gcloud  |  sed  's/github.com\/GoogleCloudPlatform\///g' | xargs > dsGcloudVersion
+#!/bin/sh
+cat go.mod| grep github.com/GoogleCloudPlatform/deploystack | head -1 | sed  's/github.com\/GoogleCloudPlatform\///g' | xargs  > versionDS
+cat go.mod| grep github.com/GoogleCloudPlatform/deploystack/tui  |  sed  's/github.com\/GoogleCloudPlatform\///g' | xargs > versionTUI
+cat go.mod| grep github.com/GoogleCloudPlatform/deploystack/gcloud  |  sed  's/github.com\/GoogleCloudPlatform\///g' | xargs > versionGcloud
+cat go.mod| grep github.com/GoogleCloudPlatform/deploystack/config  |  sed  's/github.com\/GoogleCloudPlatform\///g' | xargs > versionConfig
 date +"%m-%d-%Y %r %Z" | xargs > buildTime
