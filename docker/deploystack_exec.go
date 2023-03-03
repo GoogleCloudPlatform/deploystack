60195c4f89b74d51318c3ea4d74a0361e99c76e6
Terrence <tpryan@google.com>
2023-03-02 19:58:51 -0800

    docs: commenting the code

--- docker/deploystack_exec.go
+++ docker/deploystack_exec.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// The dsexec binary that runs on Cloud Shell to present all of the options to
+// users
 package main
 
 import (
