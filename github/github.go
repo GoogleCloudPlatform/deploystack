cc2b35a602301f06c7bcf40d237e26b0d8b5ec83
Terrence <tpryan@google.com>
2023-03-09 12:21:05 -0800

    feat: implemented New, with better ways of creating repos

--- github/github.go
+++ github/github.go
@@ -17,48 +17,58 @@
 package github
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"github.com/google/go-github/github"
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
 )
 
+// GITHUBHOST is the hostname of where to find github on the web
+const GITHUBHOST = "github.com"
+
 // Repo contains the details of a github repo for the purpose of downloading
 type Repo struct {
-	Name   string `json:"name" yaml:"name"`
-	Owner  string `json:"owner" yaml:"owner"`
-	Branch string `json:"branch" yaml:"branch"`
+	Name        string `json:"name" yaml:"name"`
+	Owner       string `json:"owner" yaml:"owner"`
+	Branch      string `json:"branch" yaml:"branch"`
+	Description string `json:"description" yaml:"description"`
 }
 
+// URL returns the github url of this repo
 func (r Repo) URL() string {
-	return fmt.Sprintf("https://github.com/%s/%s", r.Owner, r.Name)
+	return fmt.Sprintf("https://%s/%s/%s", GITHUBHOST, r.Owner, r.Name)
 }
 
+// ReferenceName is a shortcut to the reference for the current branch
 func (r Repo) ReferenceName() string {
 	return fmt.Sprintf("refs/heads/%s", r.Branch)
 }
 
 // NewRepo generates Github from a url that might contain branch information
+//
+// Deprecated: Use New() Instead
 func NewRepo(repo string) Repo {
-	result := Repo{}
+	return New("", SiteURL(repo))
+}
 
-	result.Branch = "main"
+// Populate gets metadata from github
+func (r *Repo) Populate() error {
 
-	input := strings.ReplaceAll(repo, "https://github.com/", "")
+	client := github.NewClient(nil)
 
-	sl := strings.Split(input, "/")
-	result.Owner = sl[0]
-	result.Name = sl[1]
-
-	if strings.Contains(repo, "/tree/") {
-		end := strings.Index(repo, "/tree/")
-		result.Branch = repo[end+6:]
+	repo, _, err := client.Repositories.Get(context.Background(), r.Owner, r.Name)
+	if err != nil {
+		return err
 	}
 
-	return result
+	r.Description = repo.GetDescription()
+
+	return nil
 }
 
 // Path returns where this repo should exist locally given the input path
@@ -70,7 +80,7 @@ func (r Repo) Path(path string) string {
 
 // Clone performs a git clone to the directory of our choosing
 func (r Repo) Clone(path string) error {
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
+	if _, err := os.Stat(path); err == nil {
 		return fmt.Errorf("directory (%s) already exists", path)
 	}
 
@@ -80,8 +90,56 @@ func (r Repo) Clone(path string) error {
 	}
 
 	if _, err := git.PlainClone(path, false, options); err != nil {
-		return fmt.Errorf("cannot get repo (%s) : %s", r.URL(), err)
+		return fmt.Errorf("cannot clone repo (%s) : %s", r.URL(), err)
 	}
 
 	return nil
+}
+
+// Option defines option functions that can be passed in to modify repos on New
+type Option func(*Repo)
+
+// New generates Github from a series of options
+func New(name string, options ...Option) Repo {
+	r := Repo{
+		Name:   name,
+		Owner:  "GoogleCloudPlatform",
+		Branch: "main",
+	}
+
+	for _, f := range options {
+		f(&r)
+	}
+
+	return r
+}
+
+// Owner sets the owner on a repo
+func Owner(o string) Option {
+	return func(r *Repo) {
+		r.Owner = o
+	}
+}
+
+// Branch sets the branch on a repo
+func Branch(b string) Option {
+	return func(r *Repo) {
+		r.Branch = b
+	}
+}
+
+// SiteURL sets the owner, name, and branch of a repo based on a URL
+func SiteURL(u string) Option {
+	return func(r *Repo) {
+
+		input := strings.ReplaceAll(u, fmt.Sprintf("https://%s/", GITHUBHOST), "")
+		sl := strings.Split(input, "/")
+		r.Owner = sl[0]
+		r.Name = sl[1]
+		if strings.Contains(u, "/tree/") {
+			end := strings.Index(u, "/tree/")
+			r.Branch = u[end+6:]
+		}
+
+	}
 }
