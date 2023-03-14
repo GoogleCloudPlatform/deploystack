80e3f1a15eb22c5d86bd2adf31aaaf6727ce80fc
Terrence <tpryan@google.com>
2023-03-14 09:44:47 -0700

    fix: getting version to just extract version.

--- docker/version.sh
+++ docker/version.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
-re='s/github.com\/GoogleCloudPlatform\///g'
-cat go.mod| grep deploystack | head -1 | sed  $re | xargs  > versionDS
+# Don't judge me. I'm sure there's a one liner that could do it.  Sometimes
+# you just want it to work and be done with it. 
+re1='s/github.com\/GoogleCloudPlatform\///g'
+re2='s/require//g'
+re3='s/deploystack//g'
+cat go.mod| grep deploystack | head -1 | sed  $re1 | sed  $re2 |  sed  $re3 |xargs  > versionDS
 date +"%m-%d-%Y %r %Z" | xargs > buildTime
